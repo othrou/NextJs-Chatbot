fa85f6081bb77a79ca2973d6a1dabc7b136da81b
unknown <rougui.oth@gmail.com>
2025-10-01 06:52:49 +0100

    adding rag f1

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import Footer from '@/components/main/footer';
+import ThemeButton from '@/components/main/ThemeButton';
+
 import {
   Conversation,
   ConversationContent,
@@ -31,15 +34,16 @@ import {
   Actions,
   Action,
 } from '@/components/ai-elements/actions';
+import { FileUIPart } from 'ai';
 
 import { RefreshCcwIcon, CopyIcon } from 'lucide-react';
 
 import { Fragment } from 'react';
 import { google } from '@ai-sdk/google';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { useChat } from '@ai-sdk/react';
 import { Response } from '@/components/ai-elements/response';
-import { GlobeIcon } from 'lucide-react';
+import { GlobeIcon, MicIcon } from 'lucide-react';
 import {
   Source,
   Sources,
@@ -71,6 +75,11 @@ const models = [
 const ChatBotDemo = () => {
   const [input, setInput] = useState('');
   const [model, setModel] = useState<string>(models[0].value);
+  const [useMicrophone, setUseMicrophone] = useState<boolean>(false);
+  const [files, setFiles] = useState<FileList | undefined>(undefined);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [configureOpen, setConfigureOpen] = useState(false);
+
   const [webSearch, setWebSearch] = useState(false);
   const { messages, sendMessage, status, regenerate} = useChat();
 
@@ -98,8 +107,17 @@ const ChatBotDemo = () => {
   };
 
   return (
+    
     <div className="max-w-4xl mx-auto p-6 relative size-full h-screen">
       <h1 className="text-4xl font-bold mb-4"><center> Next.js AI Chatbot By ROUGUI Othmane </center></h1>
+      <div className='flex gap-1'>
+              <ThemeButton />
+              <button onClick={() => setConfigureOpen(true)}>
+                <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M19.14 13.4006C19.18 13.1006 19.2 12.7906 19.2 12.4606C19.2 12.1406 19.18 11.8206 19.13 11.5206L21.16 9.94057C21.34 9.80057 21.39 9.53057 21.28 9.33057L19.36 6.01057C19.24 5.79057 18.99 5.72057 18.77 5.79057L16.38 6.75057C15.88 6.37057 15.35 6.05057 14.76 5.81057L14.4 3.27057C14.36 3.03057 14.16 2.86057 13.92 2.86057H10.08C9.83999 2.86057 9.64999 3.03057 9.60999 3.27057L9.24999 5.81057C8.65999 6.05057 8.11999 6.38057 7.62999 6.75057L5.23999 5.79057C5.01999 5.71057 4.76999 5.79057 4.64999 6.01057L2.73999 9.33057C2.61999 9.54057 2.65999 9.80057 2.85999 9.94057L4.88999 11.5206C4.83999 11.8206 4.79999 12.1506 4.79999 12.4606C4.79999 12.7706 4.81999 13.1006 4.86999 13.4006L2.83999 14.9806C2.65999 15.1206 2.60999 15.3906 2.71999 15.5906L4.63999 18.9106C4.75999 19.1306 5.00999 19.2006 5.22999 19.1306L7.61999 18.1706C8.11999 18.5506 8.64999 18.8706 9.23999 19.1106L9.59999 21.6506C9.64999 21.8906 9.83999 22.0606 10.08 22.0606H13.92C14.16 22.0606 14.36 21.8906 14.39 21.6506L14.75 19.1106C15.34 18.8706 15.88 18.5506 16.37 18.1706L18.76 19.1306C18.98 19.2106 19.23 19.1306 19.35 18.9106L21.27 15.5906C21.39 15.3706 21.34 15.1206 21.15 14.9806L19.14 13.4006ZM12 16.0606C10.02 16.0606 8.39999 14.4406 8.39999 12.4606C8.39999 10.4806 10.02 8.86057 12 8.86057C13.98 8.86057 15.6 10.4806 15.6 12.4606C15.6 14.4406 13.98 16.0606 12 16.0606Z" />
+                </svg>
+              </button>
+      </div>
       <div className="flex flex-col h-full">
         <Conversation className="h-full">
           <ConversationContent>
@@ -197,6 +215,15 @@ const ChatBotDemo = () => {
                   <PromptInputActionAddAttachments />
                 </PromptInputActionMenuContent>
               </PromptInputActionMenu>
+              
+              <PromptInputButton
+                onClick={() => setUseMicrophone(!useMicrophone)}
+                variant={useMicrophone ? 'default' : 'ghost'}
+              >
+                <MicIcon size={16} />
+                <span className="sr-only">Microphone</span>
+              </PromptInputButton>
+
               <PromptInputButton
                 variant={webSearch ? 'default' : 'ghost'}
                 onClick={() => setWebSearch(!webSearch)}
@@ -221,11 +248,26 @@ const ChatBotDemo = () => {
                   ))}
                 </PromptInputModelSelectContent>
               </PromptInputModelSelect>
+
+              <input
+                    type="file"
+                    onChange={(event) => {
+                      if (event.target.files) {
+                        setFiles(event.target.files);
+                      }
+                    }}
+                    multiple
+                    accept="application/pdf,image/*"
+                    ref={fileInputRef}
+                    className="hidden"
+                  />
+
             </PromptInputTools>
             <PromptInputSubmit disabled={!input && !status} status={status} />
           </PromptInputToolbar>
         </PromptInput>
       </div>
+      <Footer />
     </div>
   );
 };
